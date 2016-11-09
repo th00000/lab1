3aff738711396e64979e0c52007f21a8935afca3
zh p <zh p>
2016-11-09 17:02:58 +0800

    123

--- Lab1.java
+++ Lab1.java
@@ -1,27 +1,23 @@
 package test1;
 import java.text.SimpleDateFormat;
 import java.util.*;
-/**
- * @author pengzehao&&donghaoyang 
- */
- //多项式类
-class Polynomial {
+
+class Polynomial //多项式
+{
 	String old; //原始多项式
 	int va; //返回纯数字类型的值
-	public static final int MAX = 30;
-	public int index[]=new int[MAX]; //指数
+	int appear[]=new int[30]; //指数
 	int coe; //系数
 	boolean flag,use,out; //合法、合并、输出
-	public Polynomial(){	
-	}
-	public void clear() //初始化
+	
+	public void Clear() //初始化
 	{
-		va = 0; 
+		va=0; 
 		coe=1;
 		flag=true;
 		use=false;
 		out=false;
-		for (int i=0;i<=26;i++) index[i]=0;
+		for (int i=0;i<=26;i++) appear[i]=0;
 		return ;
 	}
 	
@@ -49,14 +45,14 @@ class Polynomial {
 		
 		for (int i=0;i<26;i++)
 		{
-			if (now.va[i]<0) ans[i]=index[i];
-			else if(now.kind==1) {ncoe*=pow(now.va[i],index[i]);ans[i]=0;}
-			else if(index[i]>0) {ncoe*=index[i];ans[i]=index[i]-1;}
+			if (now.va[i]<0) ans[i]=appear[i];
+			else if(now.kind==1) {ncoe*=pow(now.va[i],appear[i]);ans[i]=0;}
+			else if(appear[i]>0) {ncoe*=appear[i];ans[i]=appear[i]-1;}
 			if (ans[i]>0) pos=true;
 		}
 		
 		if (!pos) {va=ncoe;return;}
-		if (now.kind==2 && index[now.de]==0) {va=0;return ;}
+		if (now.kind==2 && appear[now.de]==0) {va=0;return ;}
 		if (be) System.out.print("+");
 		if (ncoe>1) {System.out.print(ncoe);st=1;}
 		for (int i=0;i<26;i++)
@@ -66,7 +62,7 @@ class Polynomial {
 				c=(char)('a'+i);
 				if (st==1) System.out.print("*");
 				st=1;
-				System.out.println(c);
+				System.out.print(c);
 				if (ans[i]>1)	System.out.print("^"+ans[i]);
 			}
 		}
@@ -74,7 +70,7 @@ class Polynomial {
 		return ;
 	}
 	
-	private int stn(String s) //将字符串转换成数字并检验合法性，逐位判断
+	private int Stn(String s) //将字符串转换成数字并检验合法性，逐位判断
 	{
 		int tmp=0,l=s.length();
 		char c;
@@ -99,10 +95,9 @@ class Polynomial {
 	{
 		String ts[]=s.split("\\^");
 		if (ts.length>2) flag=false;
-		int x;
-		int y;
-		x=stn(ts[0]);
-		if (ts.length>1) y=stn(ts[1]);
+		int x,y;
+		x=Stn(ts[0]);
+		if (ts.length>1) y=Stn(ts[1]);
 		else y=1;
 		return pow(x,y);
 	}
@@ -114,9 +109,9 @@ class Polynomial {
 		while (ts[0].charAt(be)==' ') be++;
 		if (ts[0].length()-be>1 || l>2) flag=false;
 		va=ts[0].charAt(be)-'a';
-		if (l>1) add=stn(ts[1]);
+		if (l>1) add=Stn(ts[1]);
 		else add=1;
-		if (va>=0 && va<=25) index[va]+=add; else flag=false;
+		if (va>=0 && va<=25) appear[va]+=add; else flag=false;
 		return ;
 	}
 }
@@ -140,7 +135,7 @@ class Command //指令
 	{
 		String ts[]=s.split(" ");
 		kind=1;
-		final int l=ts.length;
+		int l=ts.length;
 		if (!ts[0].equals("!simplify")) {flag=false;return ;}
 		for (int i=1;i<l;i++)	flag=Tr(ts[i]);
 		
@@ -173,7 +168,7 @@ class Command //指令
 		char c;
 		if ((l!=2) || (!ts[0].equals("!d/d"))) {flag=false;return ;}
 		c=ts[1].charAt(0);
-		if (c>='a' && c<='z') {va[c-'a']=1;de=c-'a';} else flag=false;
+		if (c>='a' || c<='z') {va[c-'a']=1;de=c-'a';} else flag=false;
 		if (ts[1].length()>1) flag=false;
 		return ;
 	}
@@ -181,11 +176,14 @@ class Command //指令
 
 public class test1 {
 	//= - =
-	public static final int MaxPoly = 500;
-	static Polynomial p[]=new Polynomial[MaxPoly];
+	static Polynomial p[]=new Polynomial[1000];
 	static Command co;
 	static int sum; //项式数
-	
+	static void Print_e() //输出错误信息
+	{
+		System.out.println("Error");
+		return ;
+	}
 	
 	static void Print_p(Command now) //输出多项式
 	{
@@ -212,13 +210,13 @@ public class test1 {
 		co.Clear();
 		co.Sim(s);
 		if (co.flag) Print_p(co);
-		else System.out.println("Error");
+		else Print_e();
 		return ;
 	}
 	
 	static boolean Judge(int de) //判断被求导的对象是否存在于多项式中
 	{
-		for (int i=0;i<sum;i++) if (p[i].index[de]>0) return true;
+		for (int i=0;i<sum;i++) if (p[i].appear[de]>0) return true;
 		return false;
 	}
 	
@@ -227,14 +225,14 @@ public class test1 {
 		co.Clear();
 		co.Der(s);
 		if (co.flag && Judge(co.de)) Print_p(co);
-		else System.out.println("Error");
+		else Print_e();
 		return ;
 	}
 	
 	static boolean Equal(int x,int y) //判断合并项
 	{
 		for (int i=0;i<25;i++)
-			if (p[x].index[i]!=p[y].index[i]) return false;
+			if (p[x].appear[i]!=p[y].appear[i]) return false;
 		return true;
 	}
 	
@@ -245,7 +243,7 @@ public class test1 {
 		sum=ts.length;
 		for (int i=0;i<sum;i++)
 		{
-			p[i].clear();
+			p[i].Clear();
 			p[i].old=ts[i];
 			p[i].Merge(ts[i]);
 			flag=flag&p[i].flag;
@@ -285,7 +283,7 @@ public class test1 {
 		while (true)
 		{
 			s=in.nextLine();
-			s=s.toLowerCase();
+			s.toLowerCase();
 			long starttime = System.currentTimeMillis();
 			//System.out.println("Start time:");
 			//Print_time();
@@ -293,7 +291,7 @@ public class test1 {
 			if (s=="End") break;
 			if (s.charAt(0)=='!')
 			{
-				if (s.length()==1 || !flag)  System.out.println("Error");
+				if (s.length()==1 || !flag)  Print_e();
 				else if (s.charAt(1)=='s')  Simplify(s);
 				else  Derivative(s);
 			}
@@ -302,7 +300,7 @@ public class test1 {
 				flag=Expression(s);
 				co.Clear();
 				if (flag) Print_p(co);
-				else  System.out.println("Error");
+				else  Print_e();
 			}
 			long endtime = System.currentTimeMillis();
 			//System.out.println("End time:");
@@ -312,7 +310,7 @@ public class test1 {
 			System.out.printf("%d ms",endtime-starttime);
 			System.out.println();
 		}
-		in.close();
+		
 	}
 	
 	static void Build()
